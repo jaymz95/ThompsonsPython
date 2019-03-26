117841cb769ea70d43b61e0595534124d87ba12a
Jaymz95 <jamesmullarkey15@gmail.com>
2019-03-26 19:18:12 +0000

    Thompsons with match and folloes functions

--- thompsons.py
+++ thompsons.py
@@ -128,7 +128,7 @@ def compile(profix):
 
 
 def followes(state):
-    """Return the set of states that can be reached from state folllowing e arrows"""
+    """Return the set of states that can be reached from state following e arrows"""
     # Create a new se, with state as its only member
     states = set()
     set.add(state)
@@ -140,6 +140,9 @@ def followes(state):
         # If there's an edge2, follow it
         states |= followes(state.edge2)
 
+    # Return the set of states
+    return states
+
 
 def match(infix, string):
     """Matches string to infix regular expression"""
@@ -149,10 +152,23 @@ def match(infix, string):
 
     # the current set of states and the next set of states
     current = set()
-    nexts = set()
+    next = set()
+
+    # Add the initial state to the current set
+    current |= followes(nfa.initial)
 
     # loop through set of character in the string
     for s in string:
+        # Loop through the current set of states
+        for c in current:
+            # Check if that state is labelled s
+            next |= followes(c.edge1)
+        # Set current to next, and clear out next
+        current = next
+        next  = set()
+
+    # Check if the accept state is in the set of current states
+    return (nfa.accept in current)
 
 # A few tests
 infixes = ["a.b.c*", "a.(b|d).c*", "(a.(b|d))*", "a.(b.b)*.c"]
