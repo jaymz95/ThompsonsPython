dde6d5084cec890410476c0390d720416d9212cd
Jaymz95 <jamesmullarkey15@gmail.com>
2019-03-26 19:34:54 +0000

    Thompsons and shunting fully working

--- thompsons.py
+++ thompsons.py
@@ -131,15 +131,19 @@ def followes(state):
     """Return the set of states that can be reached from state following e arrows"""
     # Create a new se, with state as its only member
     states = set()
-    set.add(state)
+    states.add(state)
 
     # Check id state has arrows labelled e from it
     if state.label is None:
-        # if theres an edge1, follow it
-        states |= followes(state.edge1)
-        # If there's an edge2, follow it
-        states |= followes(state.edge2)
-
+        # Check if edge1 is a state
+        if state.edge1 is not None:
+            # if theres an edge1, follow it
+            states |= followes(state.edge1)
+        # Check if edge2 is a state 
+        if state.edge2 is not None:
+            # If there's an edge2, follow it
+            states |= followes(state.edge2)
+    
     # Return the set of states
     return states
 
@@ -162,7 +166,9 @@ def match(infix, string):
         # Loop through the current set of states
         for c in current:
             # Check if that state is labelled s
-            next |= followes(c.edge1)
+            if c.label == s:
+                # Add the edge1 state to the next set
+                next |= followes(c.edge1)
         # Set current to next, and clear out next
         current = next
         next  = set()
