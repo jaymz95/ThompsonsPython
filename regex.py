8e613a9bfe781eef94ca3c229a3a3b7491680356
Jaymz95 <jamesmullarkey15@gmail.com>
2019-03-29 18:08:54 +0000

    Put test Regular Expressions in a seperate regex.py file and imported functions from shunt.py and thompsons.py

---
+++ regex.py
@@ -0,0 +1,63 @@
+# Matching Regular Expressions with Strings using Thompsons Construction and Shunting Yard Algorithm
+# James Mullarkey
+
+# Importing function from another python file
+from shunt import shunt
+# Importing function from another python file
+from thompsons import compile
+
+def followes(state):
+    """Return the set of states that can be reached from state following e arrows"""
+    # Create a new set, with state as its only member
+    states = set()
+    states.add(state)
+
+    # Check if state has arrows labelled e from it
+    if state.label is None:
+        # Check if edge1 is a state
+        if state.edge1 is not None:
+            # if theres an edge1, follow it
+            states |= followes(state.edge1)
+        # Check if edge2 is a state 
+        if state.edge2 is not None:
+            # If there's an edge2, follow it
+            states |= followes(state.edge2)
+    
+    # Return the set of states
+    return states
+
+def match(infix, string):
+    """Matches string to infix regular expression"""
+    # shunt and compile the regular expression
+    postfix = shunt(infix)
+    nfa = compile(postfix)
+
+    # the current set of states and the next set of states
+    current = set()
+    next = set()
+
+    # Add the initial state to the current set
+    current |= followes(nfa.initial)
+
+    # loop through set of character in the string
+    for s in string:
+        # Loop through the current set of states
+        for c in current:
+            # Check if that state is labelled s
+            if c.label == s:
+                # Add the edge1 state to the next set
+                next |= followes(c.edge1)
+        # Set current to next, and clear out next
+        current = next
+        next  = set()
+
+    # Check if the accept state is in the set of current states
+    return (nfa.accept in current)
+
+# A few tests
+infixes = ["a.b?","a.b.c?", "a.(b|d).c?", "(a.(b|d))*", "a.(b.b)*.c"]
+strings = ["ab", "abc", "abbc", "abcc", "abad", "abbbc"]
+
+for i in infixes:
+    for s in strings:
+        print(match(i, s), i, s)
--- thompsons.py
+++ thompsons.py
@@ -1,9 +1,6 @@
 # Thompsons Construction
 # James Mullarkey
 
-# Importing function from another python file
-from shunt import shunt
-
 # Represents a state with two arrows labelles by label
 # Use None for a label represenring "e" arrows
 class state:
@@ -114,61 +111,3 @@ def compile(profix):
 
 # print(compile("ab.cd.|"))
 # print(compile("aa.*"))
-
-
-def followes(state):
-    """Return the set of states that can be reached from state following e arrows"""
-    # Create a new set, with state as its only member
-    states = set()
-    states.add(state)
-
-    # Check if state has arrows labelled e from it
-    if state.label is None:
-        # Check if edge1 is a state
-        if state.edge1 is not None:
-            # if theres an edge1, follow it
-            states |= followes(state.edge1)
-        # Check if edge2 is a state 
-        if state.edge2 is not None:
-            # If there's an edge2, follow it
-            states |= followes(state.edge2)
-    
-    # Return the set of states
-    return states
-
-
-def match(infix, string):
-    """Matches string to infix regular expression"""
-    # shunt and compile the regular expression
-    postfix = shunt(infix)
-    nfa = compile(postfix)
-
-    # the current set of states and the next set of states
-    current = set()
-    next = set()
-
-    # Add the initial state to the current set
-    current |= followes(nfa.initial)
-
-    # loop through set of character in the string
-    for s in string:
-        # Loop through the current set of states
-        for c in current:
-            # Check if that state is labelled s
-            if c.label == s:
-                # Add the edge1 state to the next set
-                next |= followes(c.edge1)
-        # Set current to next, and clear out next
-        current = next
-        next  = set()
-
-    # Check if the accept state is in the set of current states
-    return (nfa.accept in current)
-
-# A few tests
-infixes = ["a.b?","a.b.c?", "a.(b|d).c?", "(a.(b|d))*", "a.(b.b)*.c"]
-strings = ["ab", "abc", "abbc", "abcc", "abad", "abbbc"]
-
-for i in infixes:
-    for s in strings:
-        print(match(i, s), i, s)
