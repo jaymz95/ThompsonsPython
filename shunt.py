d6f9d49009742686da727b08781f3bd7c9d00949
Jaymz95 <jamesmullarkey15@gmail.com>
2019-03-30 20:48:16 +0000

    Comments explaining code

--- shunt.py
+++ shunt.py
@@ -5,7 +5,7 @@
 def shunt(infix):
     """The Shunting Yard Algorithm for converting infix regular expressions to postfix."""
     # special characters for regular expressions and their precidence
-    specials = {'?': 50,'+': 50,'*': 50, '.': 40, '|': 30}
+    specials = {'?': 50,'+': 50,'*': 50, '.': 40, '|': 30} # dictionary
 
     # postfix will eventually be the output and operator stack
     postfix, stack = "", ""
@@ -15,14 +15,17 @@ def shunt(infix):
         if c== '(': # If an open bracket, push to the stack
             stack = stack + c
         elif c == ')': # If a closing bracket, pop from the stack, push to output until open bracket
-            while stack[-1] != '(':
+            while stack[-1] != '(': # checks if the last char in the stack is '('
                 postfix = postfix + stack[-1]
-                stack = stack[:-1]
-            stack = stack[:-1]
+                stack = stack[:-1] # taking the last char in the stack off the stack
+            stack = stack[:-1] # removes the '(' from the stack
         # If it's an operator, push to the stack after popping lower or equal precedence
         # operators from top of stack into output
         elif c in specials:
-            while stack and specials.get(c, 0) <= specials.get(stack[-1], 0):
+            # checks stack is not null and if the special character in c is lessthan 
+            # or equal to the char at the top off the stack
+            while stack and specials.get(c, 0) <= specials.get(stack[-1], 0): 
+                # takes char off the stack at puts it on the postix regex
                 postfix  = postfix + stack[-1]
                 stack = stack[:-1]
             stack = stack + c
